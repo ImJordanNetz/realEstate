ca46b84d774c6528077864218b5522d079d53790
Jordan Netz <94811498+ImJordanNetz@users.noreply.github.com>
2026-02-28 23:13:41 -0800

    testing out changing config

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			// Ensure serverless functions have access to our bundled JSON datasets
+			includeFiles: ['src/lib/server/data/**'],
+			runtime: 'nodejs20.x'
+		})
 	}
 };
 
